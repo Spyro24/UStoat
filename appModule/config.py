6c6c8625d23f0bf6b84cdca51ce28830c6a3f72b
Spyro24 <minerpi16@gmail.com>
2026-03-04 22:07:25 +0100

    end of the day

--- appModule/config.py
+++ appModule/config.py
@@ -2,10 +2,20 @@ import json
 
 class config:
     def __init__(self):
-        self.config = {}
+        self.config = {"isEncrypted": False,
+                       }
     
     def load(self, file: str):
         pass
     
     def save(self, file:str):
         pass
+    
+    def merge_if_exists(self, dict1, dict2):
+        for key in dict2:
+            if key in dict1:
+                if isinstance(dict1[key], dict) and isinstance(dict2[key], dict):
+                    self.merge_if_exists(dict1[key], dict2[key])
+                else:
+                    dict1[key] = dict2[key]
+        return dict1
