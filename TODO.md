b36c0f3eace5c1da5107437eb2dce3946e2fcaef
Spyro24 <minerpi16@gmail.com>
2026-03-06 22:37:58 +0100

    finished the encryption module

--- TODO.md
+++ TODO.md
@@ -20,6 +20,7 @@
 - [ ] Inbox for important messages (replies, mentions etc)
 - [ ] i18n support
 - [ ] performance issue fixing
+- [ ] clientside embeding perm fixe (you chose if users can embed or not, only visible for you)
 - [x] server channel selector
 - [x] server selector- [ ] server selector
 - [x] message send box 
--- appModule/s24crypt.py
+++ appModule/s24crypt.py
@@ -25,11 +25,34 @@ class s24Encryption:
         keyData = bytes()
         password = self.password.encode("UTF8")
         while lenData > len(keyData):
-            keyData += hashlib.sha256(keyData - password).digest()
+            keyData += hashlib.sha256(keyData + password).digest()
         output = b""
         n = 0
         for byte in data:
-             val = (byte + keyData[n]) & 255
+             val = (byte - keyData[n]) & 255
              output += val.to_bytes(1)
              n += 1
         return output
+    
+    def saveEncrypt(self, data: str):
+        data = self.encrypt(data)
+        data = base64.b64encode(data)
+        data = zlib.compress(data, 9)
+        data = self.encrypt(data)
+        data = base64.b64encode(data)
+        return data
+    
+    def saveDecrypt(self, data: str):
+        data = base64.b64decode(data)
+        data = self.decrypt(data)
+        data = zlib.decompress(data)
+        data = base64.b64decode(data)
+        data = self.decrypt(data)
+        return data
+
+if __name__ == "__main__":
+    test = s24Encryption()
+    test.password = "Never Gonna Give You Up"
+    encryptedData = test.saveEncrypt("If you can read this you know the password and you have cracked my encryption".encode("UTF8"))
+    print(encryptedData)
+    print(test.saveDecrypt(encryptedData))
