5a4397749090e5cabec383f999d0125047fb4447
Spyro24 <minerpi16@gmail.com>
2026-03-11 21:05:36 +0100

    added salted encryption and fixed some spacing (everybody hates my encryption because its unsafe andit not used a salt but it should be safe enough to store the user token save in the config.(aslong the user chose to encrypt the token with a password of choice and the passwort is not compromissed))

--- appModule/s24crypt.py
+++ appModule/s24crypt.py
@@ -6,48 +6,52 @@ class s24Encryption:
     def __init__(self):
         self.password = ""
     
-    def encrypt(self, data: bytes):
+    def encrypt(self, data: bytes, salt="salt"):
+        salt = salt.encode("UTF8")
         lenData = len(data)
         keyData = bytes()
         password = self.password.encode("UTF8")
         while lenData > len(keyData):
-            keyData += hashlib.sha256(keyData + password).digest()
+            keyData += hashlib.sha256(keyData + password + salt).digest()
         output = b""
         n = 0
         for byte in data:
-             val = (byte + keyData[n]) & 255
-             output += val.to_bytes(1)
-             n += 1
+            val = (byte + keyData[n]) & 255
+            output += val.to_bytes(1, byteorder="little")
+            n += 1
         return output
     
-    def decrypt(self, data: bytes):
+    def decrypt(self, data: bytes, salt="salt"):
+        salt = salt.encode("UTF8")
         lenData = len(data)
         keyData = bytes()
         password = self.password.encode("UTF8")
         while lenData > len(keyData):
-            keyData += hashlib.sha256(keyData + password).digest()
+            keyData += hashlib.sha256(keyData + password + salt).digest()
         output = b""
         n = 0
         for byte in data:
-             val = (byte - keyData[n]) & 255
-             output += val.to_bytes(1)
-             n += 1
+            val = (byte - keyData[n]) & 255
+            output += val.to_bytes(1, byteorder="little")
+            n += 1
         return output
     
-    def saveEncrypt(self, data: str):
-        data = self.encrypt(data)
+    def saveEncrypt(self, data: bytes):
+        data = self.encrypt(data, salt="UStoat")
         data = base64.b64encode(data)
+        data = self.encrypt(data, salt="Random")
         data = zlib.compress(data, 9)
-        data = self.encrypt(data)
+        data = self.encrypt(data, salt="Lookup")
         data = base64.b64encode(data)
         return data
     
     def saveDecrypt(self, data: str):
         data = base64.b64decode(data)
-        data = self.decrypt(data)
+        data = self.decrypt(data, salt="Lookup")
         data = zlib.decompress(data)
+        data = self.decrypt(data, salt="Random")
         data = base64.b64decode(data)
-        data = self.decrypt(data)
+        data = self.decrypt(data, salt="UStoat")
         return data
 
 if __name__ == "__main__":
