b69775adbac5b922ab04044f960c53ce8eb63d40
Spyro24 <minerpi16@gmail.com>
2026-06-14 22:50:42 +0200

    fck

--- loginSystem/input.py
+++ loginSystem/input.py
@@ -1,7 +1,7 @@
 import pygame as p
 
 class textInput:
-    def __init__(self, parent, font: p.Font, border=4, isPassword=False, chars=24):
+    def __init__(self, parent, font: p.Font, borderSize=4, isPassword=False, chars=24):
         self.fontSize = font.size(" ")
         self.font = font
         self.rectColor = (0, 0, 255)
@@ -9,6 +9,8 @@ class textInput:
         self.chars = chars #The lenght in chars of this input box (works only with a monospace font)
         self.isActive = False
         self.parent = parent
+        self.borderSize = borderSize
+        self.rectSize = (self.borderSize * 2 + self.fontSize[0] * self.chars, self.borderSize * 2 + self.fontSize[1])
     
     def render(self, pos: tuple[int, int]):
-        p.draw.rect(self.parent.window)
+        rect = p.draw.rect(self.parent.window, self.rectColor, (pos[0] - self))
