beb65b0f15e3d946941f85b205bd1e79da79ce64
Spyro24 <minerpi16@gmail.com>
2026-06-17 12:38:51 +0200

    added the mosfu textformater

---
+++ appModule/mosfu.py
@@ -0,0 +1,15 @@
+#mosfu, the UStoat default text formating system
+
+class mosfu:
+    def __init__(self):
+        pass
+    
+    def text_format(self, text):
+        cursiv = False
+        bold = False
+        code = False
+        textWords = test.split()
+        output = [] #this contains the formated output
+        for word in textWords:
+            if len(word) <= 1:
+                output.append(("w", word)) # w is for word and than tne word
