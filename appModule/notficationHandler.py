bf82cac8ed607c8d53474479ed68402d93675c14
Spyro24 <minerpi16@gmail.com>
2026-08-12 23:44:38 +0200

    pushed version to 0.5.0 and making the notification better

--- appModule/notficationHandler.py
+++ appModule/notficationHandler.py
@@ -28,21 +28,41 @@ class notificatonSystem:
 class notificationManager:
     def __init__(self, app: appModule.app.App):
         self.notify = notificatonSystem()
-        self.userId = app.modules['platform'].userID
+        self.runtimeStore = app.modules['runtimeStore']
+        self.userId = self.runtimeStore.userID
         self.cache = app.modules["cache"]
         self.userInMsg = "<@" + self.userId + ">"
         self.messageManager = app.modules['messageManager']
         self.messageManager.userId = self.userId
     
     def scanMessage(self, msg: dict):
-        notified = False
-        if "content" in msg and not notified:
-            content = msg["content"]
-            if self.userInMsg in content:
-                self.notify.notifyUser(content, channel=None, icon=self.cache.getUserAvatar(msg["user"]["_id"]))
-                notified = True
-        if "replies" in msg and not notified:
+        print(msg)
+        if "mentions" in msg and self.userId in msg["mentions"]:
+            msgType = "[mention]"
+            content = ""
+            if "replies" in msg:
+                msgType = "[reply]"
+            if "content" in msg:
+                newMsg = []
+                oldMsg = msg["content"].split(" ")
+                for word in oldMsg:
+                    if word.startswith("<@") and word.endswith(">"):
+                        mentionId = word[2:-1]
+                        print(mentionId)
+                        word = "@" + self.runtimeStore.users[mentionId]["username"]
+                    newMsg.append(word)
+                content = " ".join(newMsg)
+            self.notify.notifyUser(content, channel=msgType + " " + self.runtimeStore.channels[msg["channel"]]["name"], icon=self.cache.getUserAvatar(msg["user"]["_id"]))
+        '''
+        if "content" in msg:
+            for word in msg["content"].split(" "):
+                if word == self.userInMsg:
+                    self.notify.notifyUser(content, channel=self.runtimeStore.channels[msg["channel"]]["name"], icon=self.cache.getUserAvatar(msg["user"]["_id"]))
+                    return
+        if "replies" in msg:
             for replyId in msg["replies"]:
                 if replyId in self.messageManager.userMessages:
                     self.notify.notifyUser(content, channel="[reply]", icon=self.cache.getUserAvatar(msg["user"]["_id"]))
-                    notified = True
+                    return
+                
+        '''
